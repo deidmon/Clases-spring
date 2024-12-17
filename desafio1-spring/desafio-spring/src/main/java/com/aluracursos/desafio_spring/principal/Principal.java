10ce8ef9a8ddf11ed46732e9cc44c6e14d5e364f
deidmon <paul.dedios@alum.udep.edu.pe>
2024-12-17 11:36:47 -0500

    Top 10 libros más descargados

--- desafio1-spring/desafio-spring/src/main/java/com/aluracursos/desafio_spring/principal/Principal.java
+++ desafio1-spring/desafio-spring/src/main/java/com/aluracursos/desafio_spring/principal/Principal.java
@@ -1,8 +1,11 @@
 package com.aluracursos.desafio_spring.principal;
 
 import com.aluracursos.desafio_spring.Model.Datos;
+import com.aluracursos.desafio_spring.Model.DatosLibros;
 import com.aluracursos.desafio_spring.Services.ConsumoAPI;
 import com.aluracursos.desafio_spring.Services.ConvierteDatos;
+
+import java.util.Comparator;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
@@ -13,5 +16,12 @@ public class Principal {
         System.out.println(json);
         var datos = conversor.obtenerDatos(json, Datos.class);
         System.out.println(datos);
+
+        //Top 10 libros más descargados
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l -> l.titulo().toUpperCase())
+                .forEach(System.out::println);
     }
 }
