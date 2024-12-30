7cf2d3e966fd7d170dc35a84dc6eb6d0f42d80c7
deidmon <paul.dedios@alum.udep.edu.pe>
2024-12-30 12:47:53 -0500

    Mapeo de datos con DTO Java Record

--- desarrollo-api-rest/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ desarrollo-api-rest/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DatosRegistroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
     @PostMapping
-    public void registrarMedico(@RequestBody String parametro){
-        System.out.println("El request llega correctamente");
-        System.out.println(parametro);
+    public void registrarMedico(@RequestBody DatosRegistroMedico datosRegistroMedico){
+        System.out.println(datosRegistroMedico);
     }
 }
