4a680c5d6abc481460f02afba85938dbdad60d97
deidmon <paul.dedios@alum.udep.edu.pe>
2025-01-02 11:08:14 -0500

    Agregando paginación básica

--- desarrollo-api-rest/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ desarrollo-api-rest/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,10 +1,13 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DatosListadoMedico;
 import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,8 +25,9 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<Medico> listarMedicos(){
-        return medicoRepository.findAll();
+    public Page<DatosListadoMedico> listarMedicos(Pageable paginacion){
+        return medicoRepository.findAll(paginacion)
+                .map(DatosListadoMedico:: new);
     }
 
 
