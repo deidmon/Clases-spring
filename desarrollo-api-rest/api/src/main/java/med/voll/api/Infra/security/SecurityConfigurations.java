ff7db542edd121abfb625ad94d127f554f084049
deidmon <paul.dedios@alum.udep.edu.pe>
2025-01-03 12:12:31 -0500

    Configurando autenticación

--- desarrollo-api-rest/api/src/main/java/med/voll/api/Infra/security/SecurityConfigurations.java
+++ desarrollo-api-rest/api/src/main/java/med/voll/api/Infra/security/SecurityConfigurations.java
@@ -3,6 +3,8 @@ package med.voll.api.Infra.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,6 +31,11 @@ public class SecurityConfigurations {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Sesión sin estado
                 ).build();
 
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws  Exception{
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
