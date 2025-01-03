06874b6f5f72ce78f2012e22464c1a0b57a87271
deidmon <paul.dedios@alum.udep.edu.pe>
2025-01-03 14:32:23 -0500

    Retornando usuarios mysql

--- desarrollo-api-rest/api/src/main/java/med/voll/api/controller/AutenticacionController.java
+++ desarrollo-api-rest/api/src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.domain.usuarios.DatosAutenticacionUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,7 @@ public class AutenticacionController {
     private AuthenticationManager authenticationManager;
 
     @PostMapping
-    public ResponseEntity autenticarUsuario(DatosAutenticacionUsuario datosAutenticacionUsuario){
+    public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){
         Authentication token = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(),datosAutenticacionUsuario.clave());
         authenticationManager.authenticate(token);
         return ResponseEntity.ok().build();
